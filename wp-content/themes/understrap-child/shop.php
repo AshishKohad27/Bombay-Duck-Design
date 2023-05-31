74107ff22999469278f25926c23d4a82a9bef9c0
AshishKohad27 <kohadashish27@gmail.com>
2023-05-31 12:10:54 +0530

    Shop Page Responsive Error Resolve

--- wp-content/themes/understrap-child/shop.php
+++ wp-content/themes/understrap-child/shop.php
@@ -5,40 +5,49 @@
 <section class="shop-outer">
     <div class="shop-inner">
 
-        <div class="shop-img">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Shop/shop_1.png" alt="shop_1">
+        <!-- First Row -->
+        <div class="shop-firstrow">
+            <div class="shop-img">
+                <img src="./public/images/ShopCrop/shop_1.png" alt="shop_1">
+            </div>
+
+            <div class="shop-img">
+                <img src="./public/images/ShopCrop/shop_2.png" alt="shop_2">
+            </div>
+
+            <div class="shop-img">
+                <img src="./public/images/ShopCrop/shop_3.png" alt="shop_3">
+            </div>
         </div>
 
-        <div class="shop-img">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Shop/shop_2.png" alt="shop_2">
+        <!-- Second Row -->
+        <div class="shop-secondrow">
+            <div class="shop-img">
+                <img src="./public/images/ShopCrop/shop_4.png" alt="shop_4">
+            </div>
+
+            <div class="shop-img">
+                <img src="./public/images/ShopCrop/shop_5.png" alt="shop_5">
+            </div>
+
+            <div class="shop-img">
+                <img src="./public/images/ShopCrop/shop_6.png" alt="shop_6">
+            </div>
         </div>
 
-        <div class="shop-img">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Shop/shop_3.png" alt="shop_3">
-        </div>
+        <!-- Third Row -->
+        <div class="shop-thirdrow">
+            <div class="shop-img">
+                <img src="./public/images/ShopCrop/shop_7.png" alt="shop_7">
+            </div>
 
-        <div class="shop-img">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Shop/shop_4.png" alt="shop_4">
-        </div>
+            <div class="shop-img">
+                <img src="./public/images/ShopCrop/shop_8.png" alt="shop_8">
+            </div>
 
-        <div class="shop-img">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Shop/shop_5.png" alt="shop_5">
-        </div>
-
-        <div class="shop-img">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Shop/shop_6.png" alt="shop_6">
-        </div>
-
-        <div class="shop-img">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Shop/shop_7.png" alt="shop_7">
-        </div>
-
-        <div class="shop-img">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Shop/shop_8.png" alt="shop_8">
-        </div>
-
-        <div class="shop-img">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Shop/shop_9.png" alt="shop_9">
+            <div class="shop-img">
+                <img src="./public/images/ShopCrop/shop_9.png" alt="shop_9">
+            </div>
         </div>
 
     </div>
